2e9e75dcf818431f22c62938ee09a2bf17a27fc0
Sherry Avena <sherry@idebit.com>
2021-04-19 12:18:39 -0700

    testing deployment

---
+++ functions/1-hello.js
@@ -0,0 +1,14 @@
+//domain/.netlify/functions/1-hello NOTE: THE DOMAIN IS OUR IFSG SERVER BUT FOR TUTORIALS ITS LOCALHOST 8888:
+// const person = {
+//     name: 'john'
+// }
+
+exports.handler = async (event, context) => {
+
+
+    return {
+        statusCode: 200,
+        // body: JSON.stringify(person),
+        body: 'Our first netlify function example',
+    }
+}
